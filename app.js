61c45e59ef4cf3f5badf9faf372aea697273bbc0
James <james.d.trussell@gmail.com>
2023-07-06 18:49:09 -0500

    Created index.html, style.css, and app.js; Create html to add list, added styling, and used java in app.j for CRUD method for list item

---
+++ app.js
@@ -0,0 +1,55 @@
+const todoInput = document.getElementById('todo-input');
+const addTaskButton = document.getElementById('add-task-btn');
+const todoList = document.getElementById('todo-list');
+
+//Add a task
+const addTask = () => {
+    const taskText = todoInput.value.trim();
+
+    if(taskText !== '') {
+        const taskItem = createTaskItem(taskText);
+        todoList.appendChild(taskItem);
+        todoInput.value='';
+    }
+}
+//Create new task items
+const createTaskItem = (taskText) => {
+    const taskItem = document.createElement('li');
+    taskItem.className='todo-item';
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checkbox');
+
+    const taskTextSpan = document.createElement('span');
+    taskTextSpan.textContent = taskText;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', deleteTask)
+
+    taskItem.appendChild(checkbox)
+    taskItem.appendChild(taskTextSpan)
+    taskItem.appendChild(deleteBtn)
+
+    return taskItem;
+}
+//Delete task
+const deleteTask = (event) => {
+    const taskItem = event.target.parentNode;
+    todoList.removeChild(taskItem);
+}
+//Cross out task
+const toggleTask = (event)=>{
+    const taskItem = event.target.parentNode;
+    taskItem.classList.toggle('completed');
+}
+//Event listeners
+addTaskButton.addEventListener('click', addTask);
+todoInput.addEventListener('keydown', function(event) {
+    if(event.key === 'Enter') {
+        addTask();
+    }
+})
+//Examples of task
